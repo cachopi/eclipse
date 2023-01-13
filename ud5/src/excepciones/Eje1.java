71777cbed7ac8106ed746c2dd8266b6906dee3e3
cacho <cachopiccc@gmail.com>
2023-01-13 09:00:45 +0100

    java 1

---
+++ ud5/src/excepciones/Eje1.java
@@ -0,0 +1,46 @@
+package excepciones;
+
+import java.util.Scanner;
+
+public class Eje1 {
+public static void main(String[] args) {
+	
+	
+	
+
+	double n;
+boolean pedir= true;
+	int cont=0;
+	
+		do {
+	
+	try {
+		Scanner key= new Scanner(System.in);
+		
+		 n = key.nextDouble();
+		System.out.println(n);
+		pedir=false;
+		
+		
+	}catch ( Exception e) {
+		
+		
+		System.out.println("no es un numero");
+		cont++;
+		 if(cont>=5) {
+			 pedir=false;
+			 System.out.println("fin del programa");
+		 }
+		
+	}
+		
+	}while(pedir);
+	
+	
+
+	
+	
+	
+		
+}}
+
